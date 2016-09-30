e7e0b5bcc898af2347d3acfa7a63d3d1f83c8e76
Brian Recchia <brian@recchia.name>
2016-09-30 12:17:26 -0400

    modified:   Arduino-SerialCommand 	modified:   ../platformio.ini 	modified:   ../src/charduindicator.cpp

--- src/charduindicator.cpp
+++ src/charduindicator.cpp
@@ -1,52 +1,39 @@
 #include <Arduino.h>
-
+#define SERIALCOMMAND_ECHO
 #include <SerialCommand.h>
 SerialCommand sCmd;
 
 #include <Chaplex.h>
-
 byte ctrlpins[5] = {2,3,4,5,6};    //Arduino pins controlling charlieplexed leds
 #define PINS 5                //number of these pins
+
 
 Chaplex myCharlie(ctrlpins, sizeof(ctrlpins));     //control instance
 
 charlieLed myLeds[20]  = {
-                         { 0 , 1 },
-                         { 0 , 2 },
-                         { 0 , 3 },
-                         { 0 , 4 },
-                         { 1 , 0 },
-                         { 1 , 2 },
-                         { 1 , 3 },
-                         { 1 , 4 },
-                         { 2 , 0 },
-                         { 2 , 1 },
-                         { 2 , 3 },
-                         { 2 , 4 },
-                         { 3 , 0 },
-                         { 3 , 1 },
-                         { 3 , 2 },
-                         { 3 , 4 },
-                         { 4 , 0 },
-                         { 4 , 1 },
-                         { 4 , 2 },
-                         { 4 , 3 },
-                       };
-
-//long goneTime;
-
+  {1,0},{0,1},{0,2},{0,3},{0,4},
+  {2,0},{2,1},{1,2},{1,3},{1,4},
+  {3,0},{3,1},{3,2},{2,3},{2,4},
+  {4,0},{4,1},{4,2},{4,3},{3,4}
+//  { 0, 4 }, { 0, 3 }, { 0, 2 }, { 0, 1 }, { 1, 0 },
+//  { 1, 4 }, { 1, 3 }, { 1, 2 }, { 2, 1 }, { 2, 0 },
+//  { 2, 4 }, { 2, 3 }, { 3, 2 }, { 3, 1 }, { 3, 0 },
+//  { 3, 4 }, { 4, 3 }, { 4, 2 }, { 4, 1 }, { 4, 0 },
+};
 
 void unrecognized(const char *command) {
   Serial.println("What?");
 }
 
-
+void clear() {
+  myCharlie.allClear();
+}
 
 void ledOn() {
   char *ledStr;
   ledStr = sCmd.next();
   int led = atoi(ledStr);
-  led = led + 1;
+  led = led - 1;
   myCharlie.ledWrite(myLeds[led], 1);
   //myCharlie.outRow();
 }
@@ -55,19 +42,20 @@ void ledOff() {
   char *ledStr;
   ledStr = sCmd.next();
   int led = atoi(ledStr);
-  led = led + 1;
+  led = led - 1;
   myCharlie.ledWrite(myLeds[led],0);
   //myCharlie.outRow();
 }
 void setup() {
   // Initialize charlieplex array — turn off all LEDs.
-  for (byte i=0; i<20; i++){ myCharlie.ledWrite(myLeds[i], 0); }
+  myCharlie.allClear();
   myCharlie.outRow();
 
   // initialize serial:
   Serial.begin(9600);
-  sCmd.addCommand("ON", ledOn);
-  sCmd.addCommand("OFF", ledOff);
+  sCmd.addCommand("on", ledOn);
+  sCmd.addCommand("off", ledOff);
+  sCmd.addCommand("clear", clear);
   sCmd.setDefaultHandler(unrecognized);
   Serial.println("Ready");
 }
